d182bce0619c51dc30be6c1f3652b23da28c490e
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-12-01 17:24:16 +0200

    add result, full contact list

---
+++ index.js
@@ -0,0 +1,41 @@
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const deletedContact = await contacts.removeContact(id);
+      console.log(deletedContact);
+      break;
+
+    default:
+      console.warn("Unknown action type!");
+  }
+}
+
+invokeAction(argv);
